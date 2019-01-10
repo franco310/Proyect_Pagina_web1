f6fe5b7db9826766c806e00d022cd47c4306ec71
Flavio <Flavio@172.30.59.231>
2019-01-10 17:45:53 -0500

    crear proveedor 1.2

--- src/java/ReglasDeNegocio/Proveedor.java
+++ src/java/ReglasDeNegocio/Proveedor.java
@@ -5,6 +5,15 @@
  */
 package ReglasDeNegocio;
 
+import AccesoADatos.Comando;
+import AccesoADatos.Conexion;
+import AccesoADatos.Global;
+import AccesoADatos.Parametro;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Flavio
@@ -57,5 +66,195 @@ public class Proveedor {
         this.telefono = telefono;
     }
     
+    //arrays
+    public static ArrayList<Proveedor> proveedor_buscartodos() throws Exception
+    {
+         //CREO LISTA QUE RECIBIRA LOS DATOS DEL RESULSET
+        ArrayList<Proveedor> lista= new ArrayList<Proveedor>();
+          Proveedor obj1= new Proveedor();
+       ResultSet rs= null;
+      //LLAMO LA CONEXION
+      Conexion con= new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      //DECLARO UN PREPAREDSTATEMENT QUE EJECUTARA LA SQL
+      PreparedStatement preStm= null;
+
+      try {
+          //declaro mi sql
+          String sql= "select * from public.proveedor_buscartodos()";
+          //creo mi preparedstatement
+          preStm=con.creaPreparedSmt(sql);
+          //ejecuto el prepardestatement y le asigno a mi resulset  
+          rs= con.ejecutaPrepared(preStm);
+          obj1=null;
+          while (rs.next()) {
+              obj1= new Proveedor();
+              obj1.setProveedorid(rs.getInt("pproveedorid"));
+              obj1.setNombreproveedor(rs.getString("pnombreproveedor"));
+              obj1.setDireccion(rs.getString("pdireccion"));
+              obj1.setTelefono(rs.getString("ptelefono"));
+              
+              lista.add(obj1);
+          }
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
+      finally
+      {
+          rs.close();
+          preStm.close();
+          con.desconectar();
+      }
+            return lista;
+
+    }
+    
+    public static ArrayList<Proveedor> proveedor_buscarporid(int piproveedorid) throws Exception
+    {
+         //CREO LISTA QUE RECIBIRA LOS DATOS DEL RESULSET
+        ArrayList<Proveedor> lista= new ArrayList<Proveedor>();
+          Proveedor obj1= new Proveedor();
+       ResultSet rs= null;
+      //LLAMO LA CONEXION
+      Conexion con= new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      //DECLARO UN PREPAREDSTATEMENT QUE EJECUTARA LA SQL
+      PreparedStatement preStm= null;
+       
+      try {
+          //declaro mi sql
+          String sql= "select * from public.proveedor_buscarporid(?)";
+          //creo mi preparedstatement
+          preStm=con.creaPreparedSmt(sql);
+          //ejecuto el prepardestatement y le asigno a mi resulset
+          preStm.setInt(1,piproveedorid);
+          rs= con.ejecutaPrepared(preStm);
+          obj1=null;
+          while (rs.next()) {
+              obj1= new Proveedor();
+              obj1.setProveedorid(rs.getInt("pproveedorid"));
+              obj1.setNombreproveedor(rs.getString("pnombreproveedor"));
+              obj1.setDireccion(rs.getString("pdireccion"));
+              obj1.setTelefono(rs.getString("ptelefono"));
+              
+              
+              lista.add(obj1);
+          }
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
+      finally
+      {
+          rs.close();
+          preStm.close();
+          con.desconectar();
+      }
+            return lista;
+
+    }
+    
+    
+     public static boolean proveedor_insertar(Proveedor proveedor) throws Exception
+  {
+      boolean respuesta=false;
+      Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      try {
+          //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+          ArrayList<Comando> comandos = new ArrayList<Comando>();
+          //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+          Comando cmd= new Comando();
+          //SETEAMOS LA FUNCION AL COMAND0
+          cmd.setSetenciaSql("select * from public.proveedor_insertar(?,?,?,?)");
+          //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+          ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+          //llenamos el arraylist con todos los parametros
+          parametros.add(new Parametro(1, proveedor.getNombreproveedor()));
+          parametros.add(new Parametro(2, proveedor.getDireccion()));
+          parametros.add(new Parametro(3, proveedor.getTelefono()));
+          
+
+          //llenar el comando con los parametros
+          cmd.setLstParametros(parametros);
+          comandos.add(cmd);
+          //llamamos al funcion que ejecuta la transaccion en la base de datos
+          respuesta= con.ejecutaPreparedTransaccion(comandos);
+
+      } catch (SQLException e) {
+          throw  new Exception(e.getMessage());
+      }
+      finally
+      {
+          con.desconectar();
+      }
+      return respuesta;
+
+  }
+    
+     public static boolean proveedor_editar(Proveedor proveedor) throws Exception
+  {
+      boolean respuesta=false;
+      Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      try {
+          //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+          ArrayList<Comando> comandos = new ArrayList<Comando>();
+          //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+          Comando cmd= new Comando();
+          //SETEAMOS LA FUNCION AL COMAND0
+          cmd.setSetenciaSql("select * from public.proveedor_editar(?,?)");
+          //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+          ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+          //llenamos el arraylist con todos los parametros
+
+          parametros.add(new Parametro(1, proveedor.getNombreproveedor()));
+          parametros.add(new Parametro(2, proveedor.getDireccion()));
+          parametros.add(new Parametro(3, proveedor.getTelefono()));
+          
+        
+          //llenar el comando con los parametros
+          cmd.setLstParametros(parametros);
+          comandos.add(cmd);
+          //llamamos al funcion que ejecuta la transaccion en la base de datos
+          respuesta= con.ejecutaPreparedTransaccion(comandos);
+
+      } catch (SQLException e) {
+          throw  new Exception(e.getMessage());
+      }
+      finally
+      {
+          con.desconectar();
+      }
+      return respuesta;
+
+  }
+     
+      public static boolean proveedor_eliminar(int pscactbevidenid) throws Exception
+  {
+      boolean respuesta=false;
+      Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      try {
+          //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+          ArrayList<Comando> comandos = new ArrayList<Comando>();
+          //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+          Comando cmd= new Comando();
+          //SETEAMOS LA FUNCION AL COMAND0
+          cmd.setSetenciaSql("select * from public.proveedor_eliminar(?)");
+          //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+          ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+          //llenamos el arraylist con todos los parametros
+          parametros.add(new Parametro(1, pscactbevidenid));
+          //llenar el comando con los parametros
+          cmd.setLstParametros(parametros);
+          comandos.add(cmd);
+          //llamamos al funcion que ejecuta la transaccion en la base de datos
+          respuesta= con.ejecutaPreparedTransaccion(comandos);
+
+      } catch (SQLException e) {
+          throw  new Exception(e.getMessage());
+      }
+            finally
+      {
+          con.desconectar();
+      }
+      return respuesta;
+
+  }
     
 }
