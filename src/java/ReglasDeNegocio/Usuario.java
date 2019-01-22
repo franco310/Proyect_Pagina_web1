9b938a25e7e99994d9f8ae9fb9bf5750bc41a4da
Franco-Pc <Franco-Pc@Franco-Pc>
2019-01-22 12:33:14 -0500

    continuar

---
+++ src/java/ReglasDeNegocio/Usuario.java
@@ -0,0 +1,265 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ReglasDeNegocio;
+
+import AccesoADatos.Comando;
+import AccesoADatos.Conexion;
+import AccesoADatos.Global;
+import AccesoADatos.Parametro;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Franco-Pc
+ */
+public class Usuario {
+   
+    
+  private int usuarioid;
+  private String nombre_usuario;
+  private String contraseña;
+  private String cedula;
+
+    public Usuario() {
+    }
+
+    public Usuario(int usuarioid, String nombre_usuario, String contraseña, String cedula) {
+        this.usuarioid = usuarioid;
+        this.nombre_usuario = nombre_usuario;
+        this.contraseña = contraseña;
+        this.cedula = cedula;
+    }
+
+    public int getUsuarioid() {
+        return usuarioid;
+    }
+
+    public void setUsuarioid(int usuarioid) {
+        this.usuarioid = usuarioid;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+   public static ArrayList<Usuario> usuario_buscartodos() throws Exception
+    {
+         //CREO LISTA QUE RECIBIRA LOS DATOS DEL RESULSET
+        ArrayList<Usuario> lista= new ArrayList<Usuario>();
+          Usuario obj= new Usuario();
+       ResultSet rs= null;
+      //LLAMO LA CONEXION
+      Conexion con= new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      //DECLARO UN PREPAREDSTATEMENT QUE EJECUTARA LA SQL
+      PreparedStatement preStm= null;
+
+      try {
+          //declaro mi sql
+          String sql= "select * from public.usuario_buscartodos()";
+          //creo mi preparedstatement
+          preStm=con.creaPreparedSmt(sql);
+          //ejecuto el prepardestatement y le asigno a mi resulset
+          
+          rs= con.ejecutaPrepared(preStm);
+          obj=null;
+          while (rs.next()) {
+              obj= new Usuario();
+              obj.setUsuarioid(rs.getInt("pusuarioid"));
+              obj.setNombre_usuario(rs.getString("pnombre_usuario"));
+              obj.setContraseña(rs.getString("pcontraseña"));
+              obj.setCedula(rs.getString("pcedula"));
+              lista.add(obj);
+          }
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
+      finally
+      {
+          rs.close();
+          preStm.close();
+          con.desconectar();
+      }
+            return lista;
+
+    }
+    
+    public static Usuario usuario_buscarporid(int piusuarioid) throws Exception
+    {
+         //CREO LISTA QUE RECIBIRA LOS DATOS DEL RESULSET
+          Usuario obj= new Usuario();
+       ResultSet rs= null;
+      //LLAMO LA CONEXION
+      Conexion con= new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      //DECLARO UN PREPAREDSTATEMENT QUE EJECUTARA LA SQL
+      PreparedStatement preStm= null;
+       
+      try {
+          //declaro mi sql
+          String sql= "select * from public.usuario_buscarporid(?)";
+          //creo mi preparedstatement
+          preStm=con.creaPreparedSmt(sql);
+          //ejecuto el prepardestatement y le asigno a mi resulset
+          preStm.setInt(1, piusuarioid);
+          rs= con.ejecutaPrepared(preStm);
+          obj=null;
+          while (rs.next()) {
+              obj= new Usuario();
+              obj.setUsuarioid(rs.getInt("pusuarioid"));
+              obj.setNombre_usuario(rs.getString("pnombre_usuario"));
+              obj.setContraseña(rs.getString("pcontraseña"));
+              obj.setCedula(rs.getString("pcedula"));
+          }
+      } catch (SQLException e) {
+          System.out.println(e.getMessage());
+      }
+      finally
+      {
+          rs.close();
+          preStm.close();
+          con.desconectar();
+      }
+            return obj;
+
+    }
+    
+    
+     public static boolean usuario_insertar(Usuario usuario) throws Exception
+  {
+      boolean respuesta=false;
+      Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      try {
+          //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+          ArrayList<Comando> comandos = new ArrayList<Comando>();
+          //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+          Comando cmd= new Comando();
+          //SETEAMOS LA FUNCION AL COMAND0
+          cmd.setSetenciaSql("select * from public.usuario_insertar(?)");
+          //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+          ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+          //llenamos el arraylist con todos los parametros
+          parametros.add(new Parametro(1, usuario.getNombre_usuario()));
+          parametros.add(new Parametro(2, usuario.getContraseña()));
+          parametros.add(new Parametro(3, usuario.getCedula()));
+          
+
+          //llenar el comando con los parametros
+          cmd.setLstParametros(parametros);
+          comandos.add(cmd);
+          //llamamos al funcion que ejecuta la transaccion en la base de datos
+          respuesta= con.ejecutaPreparedTransaccion(comandos);
+
+      } catch (SQLException e) {
+          throw  new Exception(e.getMessage());
+      }
+      finally
+      {
+          con.desconectar();
+      }
+      return respuesta;
+
+  }
+    
+     public static boolean usuario_editar(Usuario usuario) throws Exception
+  {
+      boolean respuesta=false;
+      Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      try {
+          //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+          ArrayList<Comando> comandos = new ArrayList<Comando>();
+          //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+          Comando cmd= new Comando();
+          //SETEAMOS LA FUNCION AL COMAND0
+          cmd.setSetenciaSql("select * from public.usuario_editar(?,?)");
+          //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+          ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+          //llenamos el arraylist con todos los parametros
+
+          parametros.add(new Parametro(1, usuario.getUsuarioid()));
+          parametros.add(new Parametro(2, usuario.getNombre_usuario()));
+          parametros.add(new Parametro(3, usuario.getContraseña()));
+          parametros.add(new Parametro(4, usuario.getCedula()));
+        
+          
+          //llenar el comando con los parametros
+          cmd.setLstParametros(parametros);
+          comandos.add(cmd);
+          //llamamos al funcion que ejecuta la transaccion en la base de datos
+          respuesta= con.ejecutaPreparedTransaccion(comandos);
+
+      } catch (SQLException e) {
+          throw  new Exception(e.getMessage());
+      }
+      finally
+      {
+          con.desconectar();
+      }
+      return respuesta;
+
+  }
+     
+      public static boolean usuario_eliminar(int pscactbevidenid) throws Exception
+  {
+      boolean respuesta=false;
+      Conexion con = new Conexion(Global.driver, Global.url, Global.user, Global.pass);
+      try {
+          //CREAMOS EL ARRAYLIST DE LOS COMANDOS O SENTENCIAS SQL
+          ArrayList<Comando> comandos = new ArrayList<Comando>();
+          //CREAMOS EL PRIMER COMANDO QUE SERA AÃ‘ADIDO AL ARRAYLIST D COMANDOS
+          Comando cmd= new Comando();
+          //SETEAMOS LA FUNCION AL COMAND0
+          cmd.setSetenciaSql("select * from public.usuario_eliminar(?)");
+          //CREAMOS EL ARRALIST DE PARAMETROS PARA ASIGANR A MI PRIMER COMANDO
+          ArrayList<Parametro> parametros = new ArrayList<Parametro>();
+          //llenamos el arraylist con todos los parametros
+          parametros.add(new Parametro(1, pscactbevidenid));
+          //llenar el comando con los parametros
+          cmd.setLstParametros(parametros);
+          comandos.add(cmd);
+          //llamamos al funcion que ejecuta la transaccion en la base de datos
+          respuesta= con.ejecutaPreparedTransaccion(comandos);
+
+      } catch (SQLException e) {
+          throw  new Exception(e.getMessage());
+      }
+            finally
+      {
+          con.desconectar();
+      }
+      return respuesta;
+
+  }
+    
+}
+
+  
+  
+  
+  
+    
+    
+
